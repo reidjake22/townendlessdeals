aae4f2dc87df865b87b3ad7e3f9323e446305065
Jake <26926501+reidjake22@users.noreply.github.com>
2025-05-16 22:28:43 +0100

    added users will have to do something better than this soon

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 # Load env vars
 load_dotenv(override=True)
-
+print("INITIAL BOOT UP *************************")
 app = Flask(__name__)
 scheduler_started = False
 
